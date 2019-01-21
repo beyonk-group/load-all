6a20936e7cea21d4fd0ecc9e16734bffbf28f843
Antony Jones <antony@beyonk.com>
2019-01-21 10:50:41 +0000

    add ci config

--- index.js
+++ index.js
@@ -4,34 +4,36 @@ const proxy = require('./require-proxy')
 const { join } = require('path')
 const fs = require('fs')
 
-exports.includeDir = function (dir, kind, modifier = m => m) {
+function resovleAbsolutePath (dir, file, kind) {
+  const absolutePath = join(dir, file)
+  const isSubDirectory = fs.statSync(absolutePath).isDirectory()
+  if (isSubDirectory) {
+    if (kind) {
+      console.log(`Adding ${kind} from ${file}`)
+    }
+    return absolutePath
+  }
+  return undefined
+}
+
+function loadFiles (dir, fn, kind, initial) {
   return fs.readdirSync(dir)
     .reduce((curr, file) => {
-      const absolutePath = join(dir, file)
-      const isSubDirectory = fs.statSync(absolutePath).isDirectory()
-      if (isSubDirectory) {
-        if (kind) {
-          console.log(`Adding ${kind} from ${file}`)
-        }
-        const modified = modifier(proxy.load(absolutePath))
-        curr = curr.concat(modified)
-      }
-      return curr
-    }, [])
+      const absolutePath = resovleAbsolutePath(dir, file, kind)
+      return absolutePath ? fn(curr, absolutePath) : curr
+    }, initial)
+}
+
+exports.includeDir = function (dir, kind, modifier = m => m) {
+  return loadFiles(dir, (curr, absolutePath) => {
+    const modified = modifier(proxy.load(absolutePath))
+    return [ ...curr, modified ]
+  }, kind, [])
 }
 
 exports.exportDir = function (dir, kind, modifier = m => m) {
-  return fs.readdirSync(dir)
-    .reduce((curr, file) => {
-      const absolutePath = join(dir, file)
-      const isSubDirectory = fs.statSync(absolutePath).isDirectory()
-      if (isSubDirectory) {
-        if (kind) {
-          console.log(`Adding ${kind} from ${file}`)
-        }
-        const exported = proxy.load(absolutePath)
-        Object.assign(curr, modifier(exported))
-      }
-      return curr
-    }, {})
+  return loadFiles(dir, (curr, absolutePath) => {
+    const exported = proxy.load(absolutePath)
+    return { ...curr, ...modifier(exported) }
+  }, kind, {})
 }
